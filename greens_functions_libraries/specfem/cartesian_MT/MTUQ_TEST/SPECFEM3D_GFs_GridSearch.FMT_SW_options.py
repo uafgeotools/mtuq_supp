bb2374992182a863431d30591c4a7483d40b324d
Félix Rodríguez Cardozo <frodriguezcardozo@gmail.com>
2023-02-24 18:14:28 -0500

    Source-side MTUQ moment tensor estimation
    These files are an example for running a source-side MTUQ moment tensor estimation, calculating the Green Functions in Specfem3D Cartesian.

---
+++ greens_functions_libraries/specfem/cartesian_MT/MTUQ_TEST/SPECFEM3D_GFs_GridSearch.FMT_SW_options.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+
+import os
+from zlib import MAX_WBITS
+import numpy as np
+import argparse
+from mtuq import read, open_db, download_greens_tensors
+from mtuq.event import Origin
+from mtuq.graphics import plot_data_greens1, plot_beachball, plot_misfit_lune
+from mtuq.grid import FullMomentTensorGridSemiregular
+from mtuq.grid_search import grid_search
+from mtuq.misfit import Misfit
+from mtuq.process_data import ProcessData
+from mtuq.util import fullpath, merge_dicts, save_json
+from mtuq.util.cap import parse_station_codes, Trapezoid
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(
+    description="Input event info run MTUQ",
+    formatter_class=argparse.RawTextHelpFormatter,
+                                     )
+
+    #parser.add_argument("-mdir",type=str,help="Main dir: -mdir /Users/felix/Documents/INVESTIGACION/2_FEB_JUL_2022/IRIS_WORKSHOP/MTUQ_INVERSIONS/FK_VS_1D_CUBIC_MESH/gs_mtuq")
+    parser.add_argument("-event",type=str,help="event (event directory must be in main dir): -event 20140823183304000 ")
+    parser.add_argument("-evla",type=str,help="Event latitude: -evla 64.68 ")
+    parser.add_argument("-evlo",type=str,help="Event longitude: -evla -98.2 ")
+    parser.add_argument("-evdp",type=float,help="Event depth in m: -evdp 10000.0 ")
+    parser.add_argument("-mw",type=float,help="Event magnitude: -mw 4.7")
+    parser.add_argument("-time", type=str,help="Earthquake origin time: -time 2014-08-25T16:19:03.00000Z")
+    parser.add_argument("-np", type=int,help="Number of points per axis: -np 10")
+    parser.add_argument("-fb",type=str,help="Frequency band for filtering data in seconds: -fb 10-100")
+
+    return parser.parse_args()
+
+
+if __name__=='__main__':
+    #
+    # Carries out grid search over all moment tensor parameters for the catalog depth and magnitude of the 2020-04-04 SoCal event
+    # Uses local database of Green's functions calculated via the FK method
+    #
+    # USAGE
+    #   mpirun -n <NPROC> python GridSearch.FullMomentTensor_cat_SOCAL.py
+    #
+
+    param = parse_args()
+    print(param.event)
+    mdir = os.getcwd()
+
+    path_data=    fullpath('{}/{}/*.[zrt]'.format(mdir,param.event))
+    path_weights= fullpath('{}/{}/weights.dat'.format(mdir,param.event))
+    event_id=     '{}'.format(param.event)
+    model=        'ak135'
+    #db = open_db('{}/greens/ir'.format(param.mdir),format='FK')
+    depth = int(param.evdp/1000)
+    db = open_db('GFs/{}/{}'.format(event_id,depth),format="SPECFEM3D")
+
+    #
+    # Body and surface wave measurements will be made separately
+    #
+
+    freqs = param.fb.split('-')
+    process_sw = ProcessData(
+        filter_type='Bandpass',
+        freq_min=1/float(freqs[1]),
+        freq_max=1/float(freqs[0]),
+        pick_type='taup',
+        taup_model=model,
+        #pick_type='FK_metadata',
+        #FK_database='{}/greens/ir'.format(param.mdir),
+        window_type='surface_wave',
+        window_length=250.,
+        capuaf_file=path_weights,
+        )
+
+    #
+    # For our objective function, we will use a sum of body and surface wave
+    # contributions
+    #
+
+    misfit_sw = Misfit(
+        norm='L2',
+        time_shift_min=-15.,
+        time_shift_max=+15.,
+        time_shift_groups=['ZR','T'],
+        )
+
+    #
+    # User-supplied weights control how much each station contributes to the
+    # objective function
+    #
+    station_id_list = parse_station_codes(path_weights)
+
+    #
+    # Next, we specify the moment tensor grid and source-time function
+    #
+
+    #magnitudes=np.arange(param.mw-0.2,param.mw+0.2,0.1)
+
+    grid = FullMomentTensorGridSemiregular(
+        npts_per_axis=param.np,
+        #magnitudes= magnitudes.tolist()
+        magnitudes=[param.mw]
+        )
+
+    wavelet = Trapezoid(
+        magnitude=param.mw)
+
+    #
+    # Origin time and location will be fixed. For an example in which they
+    # vary, see examples/GridSearch.DoubleCouple+Magnitude+Depth.py
+    #
+    # See also Dataset.get_origins(), which attempts to create Origin objects
+    # from waveform metadata
+    #
+
+    origin = Origin({
+        'time': '{}'.format(param.time),
+        'latitude': param.evla,
+        'longitude': param.evlo,
+        'depth_in_m': param.evdp,
+        })
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+
+    #
+    # The main I/O work starts now
+    #
+
+    if comm.rank==0:
+        print('Reading data...\n')
+        data = read(path_data, format='sac',
+            event_id=event_id,
+            station_id_list=station_id_list,
+            tags=['units:cm', 'type:velocity'])
+
+
+        data.sort_by_distance()
+        stations = data.get_stations()
+
+
+        print('Processing data...\n')
+        data_sw = data.map(process_sw)
+
+
+        print('Reading Greens functions...\n')
+        greens = db.get_greens_tensors(stations,origin)
+
+        print('Processing Greens functions...\n')
+        greens.convolve(wavelet)
+        greens_sw = greens.map(process_sw)
+
+
+    else:
+        stations = None
+        data_sw = None
+        greens_sw = None
+
+
+    stations = comm.bcast(stations, root=0)
+    data_sw = comm.bcast(data_sw, root=0)
+    greens_sw = comm.bcast(greens_sw, root=0)
+
+    #
+    # The main computational work starts now
+    #
+
+
+    if comm.rank==0:
+        print('Evaluating surface wave misfit...\n')
+
+    results_sw = grid_search(
+        data_sw, greens_sw, misfit_sw, origin, grid)
+
+
+    if comm.rank==0:
+
+        results = results_sw
+
+        # array index corresponding to minimum misfit
+        #idx = results.idxmin('source') #Old version
+        idx = results.source_idxmin() #New version
+
+        best_source = grid.get(idx)
+        lune_dict = grid.get_dict(idx)
+        mt_dict = grid.get(idx).as_dict()
+
+
+        #
+        # Generate figures and save results
+        #
+
+        print('Generating figures...\n')
+
+        plot_data_greens1(event_id+'FMT_cat_waveforms.png',
+            data_sw, greens_sw, process_sw,
+            misfit_sw, stations, origin, best_source, lune_dict)
+
+
+        plot_beachball(event_id+'FMT_cat_beachball.png',
+            best_source, stations, origin)
+
+
+        plot_misfit_lune(event_id+'FMT_cat_misfit.png', results)
+        plot_misfit_lune(event_id+'FMT_cat_misfit_mt.png', results, show_mt=True)
+        plot_misfit_lune(event_id+'FMT_cat_misfit_tradeoff.png', results, show_tradeoffs=True)
+
+        print('Saving results...\n')
+
+        merged_dict = merge_dicts(lune_dict, mt_dict, origin,
+            {'M0': best_source.moment(), 'Mw': best_source.magnitude()})
+
+
+        # save best-fitting source
+        save_json(event_id+'FMT_cat_solution.json', merged_dict)
+
+
+        # save misfit surface
+        results.save(event_id+'FMT_cat_misfit.nc')
+
+
+        print('\nFinished\n')
---
+++ greens_functions_libraries/specfem/cartesian_MT/SOLVER_REPO/20171201023244/15/Mrp/schedule_solver.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#SBATCH --job-name=specfem3D
+#SBATCH --output=specfem3D.%j.output.txt
+#SBATCH --time=04:00:00
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-gpu=4096
+#SBATCH --partition=snsm_itn19
+#SBATCH --qos=openaccess
+#SBATCH --gres=gpu:1
+
+#export SLURM_OVERLAP=1
+#ulimit unlimited
+
+#below lines used for GCC4/OpenMPI 2.0.4 build of specfem3d_devel
+module purge
+module load apps/specfem3d/3.0.1i
+#export CUDA_LIB=/apps/cuda/10.1/targets/x86_64-linux/lib
+#export LD_LIBRARY_PATH=/usr/lib64/psm2-compat:$LD_LIBRARY_PATH
+#export PSM2_MULTI_EP=1
+
+
+##################################
+# Explanation of above parameters
+##
+# job-name: What will the job be called when you use `squeue`
+# output: What will the Slurm stdout file be called
+# time: How much time ( hour:minutes:seconds ) will the job be allotted
+# nodes: How many nodes ( max 20 when using GPUs ) will be allotted
+# ntasks-per-node: How many threads will you need per node (max 2 when using GPUs)
+# mem: How much memory (RAM) will you be allotted per node ( defined in MBs, default 512)
+# partition: what partition of CIRCE do you want to run on? Need to specify one with GPUs if doing a GPU job
+# qos=openaccess: required for GPU jobs
+# gres=gpu:# : Used for GPU jobs to determine how many GPUs are assigned (max 2, should match ntasks?)
+##################################
+
+logfile=$SLURM_JOB_NAME"."$SLURM_JOB_ID.output.txt
+echo "TIME BEGAN: " >> $logfile
+date >> $logfile
+SECONDS=0
+
+# cleans output files
+#mkdir -p OUTPUT_FILES
+#rm -rf OUTPUT_FILES/*
+
+# stores setup
+cp DATA/meshfem3D_files/Mesh_Par_file OUTPUT_FILES/
+cp DATA/Par_file OUTPUT_FILES/
+cp DATA/CMTSOLUTION OUTPUT_FILES/
+cp DATA/STATIONS OUTPUT_FILES/
+
+echo "running example: `date`"
+currentdir=`pwd`
+
+# get the number of processors, ignoring comments in the Par_file
+NPROC=`grep ^NPROC DATA/Par_file | grep -v -E '^[[:space:]]*#' | cut -d = -f 2`
+
+BASEMPIDIR=`grep ^LOCAL_PATH DATA/Par_file | cut -d = -f 2 `
+
+# runs simulation
+# This is a MPI simulation
+echo
+echo "  running solver on $NPROC processors..."
+echo
+mpirun -np $NPROC xspecfem3D
+
+# checks exit code
+if [[ $? -ne 0 ]]; then exit 1; fi
+
+echo
+echo "see results in directory: OUTPUT_FILES/"
+echo
+echo "done"
+echo `date`
+
+
+# To create a full mesh using combine_vol_data:
+# cd bin
+# xcombine_vol_data 0 0 vs ../OUTPUT_FILES/DATABASES_MPI/ ../OUTPUT_FILES/DATABASES_MPI/ 1
+# cd ../OUTPUT_FILES/DATABASES_MPI/
+# (check that mesh2vtu.pl is working)
+# ../../../../../utils/Visualization/Paraview/mesh2vtu.pl -i vs.mesh -o vs.vtu
+#
